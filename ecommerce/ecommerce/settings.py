14df3f024cf4ba982099e95ed1d9f17341ee8269
ayslenon <ayslenon.c@gmail.com>
2022-11-03 15:36:00 -0300

    configurando postgresql

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postegresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CkXglmuC8kPcZjFdjNjA',
+        'HOST': 'containers-us-west-21.railway.app',
+        'PORT': '6276',
     }
 }
 
